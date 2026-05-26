73d5d1bb527dda7a38a8d16e292c384320236349
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2026-05-26 11:46:20 +0200

    Fix for JacksonDatabind

--- promptBasic.sh
+++ promptBasic.sh
@@ -48,6 +48,14 @@ do
 		sed -i 's/<maven.compile.source>1.1<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.1<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
 		sed -i '/<artifactId>junit<\/artifactId>/{n;s/<version>3.8.1<\/version>/<version>4.13.2<\/version>/}' $PROJECTFOLDER/pom.xml
 		
+		# Fix for JacksonDatabind
+		if [ -f $PROJECTFOLDER/src/main/java/com/fasterxml/jackson/databind/cfg/PackageVersion.java ]; then
+			rm -f $PROJECTFOLDER/src/main/java/com/fasterxml/jackson/databind/cfg/PackageVersion.java
+			COMPILER_PLUGIN="<plugin><groupId>org.apache.maven.plugins</groupId><artifactId>maven-compiler-plugin</artifactId><version>2.5.1</version><configuration><source>1.8</source><target>1.8</target><showWarnings>false</showWarnings><failOnWarning>false</failOnWarning><compilerArgument>-Xlint:-deprecation</compilerArgument></configuration></plugin>"
+
+			sed -i.bak "/<plugins>/a $COMPILER_PLUGIN" $PROJECTFOLDER/pom.xml
+		fi
+		
 		cat $PROJECTFOLDER/pom.xml | grep "maven.compile"
 
 		(cd $PROJECTFOLDER/ && mvn clean test) &> runs/before_"$BUG".txt
