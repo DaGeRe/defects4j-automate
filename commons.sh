5006e284a7ea34f0a73bf1edae041a13d4a4501c
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2026-07-13 17:58:45 +0200

    Add script for getting the call tree size

--- commons.sh
+++ commons.sh
@@ -10,3 +10,26 @@ get_bug_statistics() {
 	
 	awk 'BEGIN {fixed=0;} {if ($3 == 0) fixed++; if ($2 == 1) overall++;} END {print fixed" / "overall " (" fixed/overall")";}' "$file_path"
 }
+
+fixPomXML() {
+	PROJECTFOLDER=$1
+	echo "Fixing pom.xml"
+	sed -i 's/<maven.compile.source>1.6<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.6<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
+	sed -i 's/<maven.compile.source>1.5<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.5<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
+	sed -i 's/<maven.compile.source>1.3<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.3<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
+	sed -i 's/<maven.compile.source>1.2<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.2<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
+	sed -i 's/<maven.compile.source>1.1<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.1<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
+	sed -i '/<artifactId>junit<\/artifactId>/{n;s/<version>3.8.1<\/version>/<version>4.13.2<\/version>/}' $PROJECTFOLDER/pom.xml
+	sed -i 's|<source>1.5</source>|<source>1.8</source>|g' $PROJECTFOLDER/pom.xml
+	sed -i 's|<target>1.5</target>|<target>1.8</target>|g' $PROJECTFOLDER/pom.xml
+	
+	# Fix for JacksonDatabind
+	if [ -f $PROJECTFOLDER/src/main/java/com/fasterxml/jackson/databind/cfg/PackageVersion.java ]; then
+		rm -f $PROJECTFOLDER/src/main/java/com/fasterxml/jackson/databind/cfg/PackageVersion.java
+		COMPILER_PLUGIN="<plugin><groupId>org.apache.maven.plugins</groupId><artifactId>maven-compiler-plugin</artifactId><version>2.5.1</version><configuration><source>1.8</source><target>1.8</target><showWarnings>false</showWarnings><failOnWarning>false</failOnWarning><compilerArgument>-Xlint:-deprecation</compilerArgument></configuration></plugin>"
+
+		sed -i.bak "/<plugins>/a $COMPILER_PLUGIN" $PROJECTFOLDER/pom.xml
+	fi
+	
+	cat $PROJECTFOLDER/pom.xml | grep "maven.compile"
+}
---
+++ getCallTreeSizes.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+export PROJECT="${1:-Lang}"
+
+source commons.sh
+
+if [ "$PROJECT" == "Lang" ]; then
+	export KIEKER_SIGNATURES_INCLUDE="org.apache.commons.lang3.*"
+fi
+if [ "$PROJECT" == "JacksonDatabind" ]; then
+	export KIEKER_SIGNATURES_INCLUDE="com.fasterxml.jackson.databind.*"
+fi
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+if [ ! -f ../defects4j/framework/projects/$PROJECT/active-bugs.csv ]; then
+	echo "File ../defects4j/framework/projects/$PROJECT/active-bugs.csv not found; project needs to be existing."
+	exit 1
+fi
+
+BUGS=$(cat ../defects4j/framework/projects/$PROJECT/active-bugs.csv | awk -F"," '{print $1}' | grep -v "bug.id")
+
+echo $BUGS
+mv bugs_"$PROJECT".txt bugs_"$PROJECT"_old.txt
+
+runfolder="runs_"$PROJECT
+mkdir -p $runfolder
+
+if [ ! -f kieker-2.0.2-bytebuddy.jar ]; then
+	echo "kieker-2.0.2-bytebuddy.jar missing"
+	wget https://repo1.maven.org/maven2/net/kieker-monitoring/kieker/2.0.2/kieker-2.0.2-bytebuddy.jar
+fi
+
+for BUG in $BUGS
+do
+	rm -r /tmp/kieker* 
+	PROJECTFOLDER=/tmp/"$PROJECT"_"$BUG"_buggy
+	if [ ! -d $PROJECTFOLDER ]
+	then
+		# use "$BUG"f to get the fixed version
+		defects4j checkout -p $PROJECT -v "$BUG"f -w $PROJECTFOLDER
+		echo "target" >> $PROJECTFOLDER/.gitignore
+	fi
+	
+	if [ -f $PROJECTFOLDER/pom.xml ]
+	then
+		fixPomXML $PROJECTFOLDER
+		test="NumberUtilsTest"
+		
+		echo "Test: $test"
+		if [ -z "$test" ]
+		then
+			echo "No failing tests; skipping bug $BUG"
+			echo "$BUG skipped_no_failing_test" >> bugs"_"$PROJECT.txt
+		else
+			echo "Getting tree for $test in $BUG"
+			sed -i '/<dependencies>/a <dependency><groupId>org.slf4j</groupId><artifactId>slf4j-simple</artifactId><version>2.0.18</version><scope>test</scope></dependency>' $PROJECTFOLDER/pom.xml
+			xmlstarlet ed -L \
+				-s "//*[local-name()='plugin'][*[local-name()='artifactId']='maven-surefire-plugin'][not(*[local-name()='configuration'])]" \
+				-t elem -n "configuration" -v ""   \
+				-s "//*[local-name()='plugin'][*[local-name()='artifactId']='maven-surefire-plugin']/*[local-name()='configuration'][not(*[local-name()='argLine'])]" \
+				-t elem -n "argLine" \
+				-v "-javaagent:"$(pwd)"/kieker-2.0.2-bytebuddy.jar" \
+				$PROJECTFOLDER/pom.xml
+			echo "KIEKER_SIGNATURES_INCLUDE: $KIEKER_SIGNATURES_INCLUDE"
+			(cd $PROJECTFOLDER/ && mvn clean test -Dtest=$test) &> $runfolder/gettrace_"$BUG".txt
+			
+			echo -n "TraceLength="
+			cat /tmp/kieker*/kieker*.dat | wc -l
+		fi
+	fi
+done
