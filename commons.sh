3a84d85f6e63b5a8cd891ad59e727ee448835393
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2026-07-16 10:33:40 +0200

    Use camel_case for local variables

--- commons.sh
+++ commons.sh
@@ -41,36 +41,36 @@ getTestFromLogfile() {
 }
 
 fixPomXML() {
-	PROJECTFOLDER=$1
+	project_folder=$1
 	bug_id=$2
 	echo "Fixing pom.xml"
-	sed -i 's/<maven.compile.source>1.6<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.6<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
-	sed -i 's/<maven.compile.source>1.5<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.5<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
-	sed -i 's/<maven.compiler.source>1.5<\/maven.compiler.source>/<maven.compiler.source>1.8<\/maven.compiler.source>/g; s/<maven.compiler.target>1.5<\/maven.compiler.target>/<maven.compiler.target>1.8<\/maven.compiler.target>/g' $PROJECTFOLDER/pom.xml
-	sed -i 's/<maven.compile.source>1.4<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.4<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
-	sed -i 's/<maven.compile.source>1.3<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.3<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
-	sed -i 's/<maven.compile.source>1.2<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.2<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
-	sed -i 's/<maven.compile.source>1.1<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.1<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTFOLDER/pom.xml
-	sed -i '/<artifactId>junit<\/artifactId>/{n;s/<version>3.8.1<\/version>/<version>4.13.2<\/version>/}' $PROJECTFOLDER/pom.xml
-	sed -i 's|<source>1.5</source>|<source>1.8</source>|g' $PROJECTFOLDER/pom.xml
-	sed -i 's|<target>1.5</target>|<target>1.8</target>|g' $PROJECTFOLDER/pom.xml
+	sed -i 's/<maven.compile.source>1.6<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.6<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $project_folder/pom.xml
+	sed -i 's/<maven.compile.source>1.5<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.5<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $project_folder/pom.xml
+	sed -i 's/<maven.compiler.source>1.5<\/maven.compiler.source>/<maven.compiler.source>1.8<\/maven.compiler.source>/g; s/<maven.compiler.target>1.5<\/maven.compiler.target>/<maven.compiler.target>1.8<\/maven.compiler.target>/g' $project_folder/pom.xml
+	sed -i 's/<maven.compile.source>1.4<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.4<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $project_folder/pom.xml
+	sed -i 's/<maven.compile.source>1.3<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.3<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $project_folder/pom.xml
+	sed -i 's/<maven.compile.source>1.2<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.2<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $PROJECTproject_folderFOLDER/pom.xml
+	sed -i 's/<maven.compile.source>1.1<\/maven.compile.source>/<maven.compile.source>1.8<\/maven.compile.source>/g; s/<maven.compile.target>1.1<\/maven.compile.target>/<maven.compile.target>1.8<\/maven.compile.target>/g' $project_folder/pom.xml
+	sed -i '/<artifactId>junit<\/artifactId>/{n;s/<version>3.8.1<\/version>/<version>4.13.2<\/version>/}' $project_folder/pom.xml
+	sed -i 's|<source>1.5</source>|<source>1.8</source>|g' $project_folder/pom.xml
+	sed -i 's|<target>1.5</target>|<target>1.8</target>|g' $project_folder/pom.xml
 	
 	# Fix for JacksonDatabind
-	if [ -f $PROJECTFOLDER/src/main/java/com/fasterxml/jackson/databind/cfg/PackageVersion.java ]; then
-		rm -f $PROJECTFOLDER/src/main/java/com/fasterxml/jackson/databind/cfg/PackageVersion.java
+	if [ -f $project_folder/src/main/java/com/fasterxml/jackson/databind/cfg/PackageVersion.java ]; then
+		rm -f $project_folder/src/main/java/com/fasterxml/jackson/databind/cfg/PackageVersion.java
 		COMPILER_PLUGIN="<plugin><groupId>org.apache.maven.plugins</groupId><artifactId>maven-compiler-plugin</artifactId><version>2.5.1</version><configuration><source>1.8</source><target>1.8</target><showWarnings>false</showWarnings><failOnWarning>false</failOnWarning><compilerArgument>-Xlint:-deprecation</compilerArgument></configuration></plugin>"
 
-		sed -i.bak "/<plugins>/a $COMPILER_PLUGIN" $PROJECTFOLDER/pom.xml
+		sed -i.bak "/<plugins>/a $COMPILER_PLUGIN" $project_folder/pom.xml
 	fi
 	
-	SEVEN_T_TEST_FILE="$PROJECTFOLDER/src/test/java/org/apache/commons/compress/archivers/sevenz/SevenZNativeHeapTest.java"
+	SEVEN_T_TEST_FILE="$project_folder/src/test/java/org/apache/commons/compress/archivers/sevenz/SevenZNativeHeapTest.java"
 	if [ "$bug_id" == "41" ] && [ -f "$SEVEN_T_TEST_FILE" ]; then
 		ANNOTATION="@org.powermock.core.classloader.annotations.PowerMockIgnore({\"jdk.internal.reflect.*\", \"java.lang.*\", \"java.util.*\"})"
 		
 		sed -i "/public class SevenZNativeHeapTest/i $ANNOTATION" "$SEVEN_T_TEST_FILE"
 	fi
 	
-	if [[ "$PROJECTFOLDER" == */Compress* ]] && [ "$bug_id" -gt 8 ]; then
+	if [[ "$project_folder" == */Compress* ]] && [ "$bug_id" -gt 8 ]; then
 		plugin_block='      <plugin>
         <artifactId>maven-surefire-plugin</artifactId>
         <configuration>
@@ -80,7 +80,7 @@ fixPomXML() {
 		awk -v block="$plugin_block" '/<build>/ { print; in_build = 1; next }
     in_build && /<plugins>/ { print; print block; in_build = 0; next }
     { print }
-			' $PROJECTFOLDER/pom.xml > $PROJECTFOLDER/pom.xml.tmp && mv $PROJECTFOLDER/pom.xml.tmp $PROJECTFOLDER/pom.xml
+			' $project_folder/pom.xml > $project_folder/pom.xml.tmp && mv $project_folder/pom.xml.tmp $project_folder/pom.xml
 	fi
-	cat $PROJECTFOLDER/pom.xml | grep "maven.compile"
+	cat $project_folder/pom.xml | grep "maven.compile"
 }
